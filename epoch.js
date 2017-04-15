ca0fef61d7b2966edc713b8e709f008dc80a5c6b
Naveen Pai <naveen.pai@bloomreach.com>
2017-04-15 17:25:41 +0000

    Initial commit adding code for addon

---
+++ epoch.js
@@ -0,0 +1,13 @@
+chrome.contextMenus.create({
+    title: "Convert %s to Readable Time", 
+    contexts:["selection"], 
+    onclick: function(info, tab) {
+        var d = new Date(0);
+        if(info.selectionText.length >= 12){
+            d.setUTCMilliseconds(info.selectionText);
+        } else {
+            d.setUTCSeconds(info.selectionText);
+        }
+        alert(d.toUTCString());
+    }
+});
